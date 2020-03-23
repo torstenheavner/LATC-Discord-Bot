5654997c3fbb194cc86e5b304ca1f07c41a50bfe
Toaster <ttoasterrr@gmail.com>
2020-03-23 09:43:36 -0600

    shurg

---
+++ LATC.py
@@ -0,0 +1,59 @@
+import sys
+
+from discord.ext import commands
+
+import useful as u
+
+sys.path.append("T:/all")
+bot = commands.Bot(command_prefix=".")
+
+cogs = u.getData()["cogs"]
+u.clear()
+for extension in cogs:
+    bot.load_extension(extension)
+    u.nprint("GREEN", "MAIN", "%s INITIALIZED" % extension.upper())
+
+
+@bot.event
+async def on_connect():
+    u.nprint("GREEN", "MAIN", "CONNTECTED")
+
+
+@bot.event
+async def on_disconnect():
+    u.nprint("GREEN", "MAIN", "DISCONNECTED")
+
+
+@bot.event
+async def on_ready():
+    u.nprint("GREEN", "MAIN", "ONLINE")
+    u.nprint("GREEN", "MAIN", "ALL PROCESSES INITIALIZED SUCCESFULLY")
+
+
+@bot.command(brief="Check if the bot is online")
+async def ping(ctx):
+    await ctx.send("Pong! The bot is online.")
+    u.cprint("GREEN", "MAIN", ctx, "BOT PINGED")
+
+
+@bot.command(brief="Reload all of the bots modules (update the code)")
+async def reload(ctx):
+    u.cprint("GREEN", "MAIN", ctx, "PROCESSES RE-INITIALIZED")
+    log = []
+    _cogs = u.getData()["cogs"]
+    for _extension in _cogs:
+        try:
+            bot.reload_extension(_extension)
+            u.nprint("GREEN", "MAIN", "%s RE-INITIALIZED" % _extension.upper())
+            log.append("\"**%s**\" re-initialized" % _extension)
+        except:
+            bot.load_extension(_extension)
+            u.nprint("GREEN", "MAIN", "%s INITIALIZED" % _extension.upper())
+            log.append("\"**%s**\" initialized" % _extension)
+    u.nprint("GREEN", "MAIN", "ALL PROCESSES RUNNING")
+
+    await ctx.send("\n".join(log))
+
+
+with open("T:/all/latc_creds.txt", "r") as token:
+    bot.run(token.read())
---
+++ general.py
@@ -0,0 +1,28 @@
+import random
+from importlib import *
+
+from discord.ext import commands
+
+import useful as u
+
+
+class General(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.color = "MAGENTA"
+        self.name = "GENERAL"
+
+    def cog_unload(self):
+        u.print(self.color, self.name, "OFFLINE")
+
+    @commands.command(brief="Call someone cringe")
+    async def cringe(self, ctx):
+        await ctx.delete()
+        await ctx.send(random.choice(["that was cringe,,,,", "cringe.", "stop,, dont do that ever again", "that was so cringe", "i can confirm that this is cringe", "you are big cringe", "uh oh, big cringe", "you are unlike the fred movie trilogy,\nwhich is to say, you are cringe."]))
+        u.cprint(self.color, self.name, ctx, "SOMEONE WAS CRINGE")
+
+
+def setup(bot):
+    u.nprint("MAGENTA", "GENERAL", "ONLINE")
+    bot.add_cog(General(bot))
+    reload(u)
